0921ab3a44f885d89a1237684d40322d3531c19c
fengyun2 <2816708306@qq.com>
2016-10-13 16:04:05 +0800

    create a vue-server-render-bio

---
+++ README.md
@@ -0,0 +1,9 @@
+# vue-server-render-bio
+
+> vue-server-render-bio
+
+```bash
+npm install
+npm run build
+npm start
+```
---
+++ src/index.js
@@ -0,0 +1,8 @@
+/*
+* @Author: baby
+* @Date:   2016-10-13 14:01:49
+* @Last Modified by:   baby
+* @Last Modified time: 2016-10-13 14:10:54
+*/
+
+'use strict';
